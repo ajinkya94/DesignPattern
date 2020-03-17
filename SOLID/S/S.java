1f9e383fa7bbd6880721525a784c08659c1fa50f
ajinkya94 <ajinkya1994.gmail.com>
2020-03-16 22:02:54 -0700

    first commit to remote

---
+++ SOLID/S/S.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+import java.net.URL;
+
+class Journal{
+    private final List<String> entries = new ArrayList<>();
+    private static int count = 0;
+    public void addEntry(String text){
+        entries.add(""+(count++)+" : "+text);
+    }
+    public void removeEntry(int index){
+        entries.remove(index);
+    }
+
+
+    @Override
+    public String toString(){
+        return String.join(System.lineSeparator(),entries);
+    }
+
+    /* Here, we are implementing a multiple functions that are related to this class but makes the class more baggy and prevents --> (S)OLID  <---.
+    IDEA - we can implement 'persistent' in a seperate class which saves the data into a seperate file*/
+    public void loadIntoFile(String filename){ }
+    public void loadFromFile(URL url) { }
+
+}
+/* This is the solution for the above problem. Seperation of concerns*/
+class Persistence{
+    public void saveToFile(Journal journal,String fileName, boolean overwrite) throws FileNotFoundException{
+        if(overwrite || new File(fileName).exists()){
+            try(PrintStream out = new PrintStream(fileName)){
+                out.println(journal.toString());
+            }
+            catch(FileNotFoundException e){
+                System.out.println(e);
+            }
+        }
+    }
+    public void loadFile(String fileName){ }
+    public void loadWeb(URL url){ }
+
+}
+
+
+class Main {
+    public static void main(String[] args) throws Exception{
+        Journal j = new Journal();
+        j.addEntry("I am angry today");
+        j.addEntry("I hate bugs");
+        System.out.println(j);
+        Persistence p = new Persistence();
+        String fileName = "journal.txt";
+        p.saveToFile(j, fileName, true);
+    }
+}
